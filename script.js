112cd75239a91fd9dbcb64449de2ccc1223a83e6
Saurabh Pal <118064701+saurabh27pal@users.noreply.github.com>
2023-01-25 11:52:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+//your code here
+let textTag=document.getElementById('evaluatedText');
+let headerTag=document.getElementById('letterCount');
+
+document.getElementById('evaluatedText').addEventListener("keyup",function(){
+let textValue=textTag.value;
+headerTag.innerHTML=textValue.length;
+
+})
